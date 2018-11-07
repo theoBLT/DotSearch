b20b3a33a967694a26e582fa6abe3ab4fd4f4cc1
theoBLT <theobloc@gmail.com>
2018-11-07 08:07:35 +0100

    Removed records limitation

--- env-example.js
+++ env-example.js
@@ -1,3 +1,3 @@
 var
-  airtableUrl = "https://api.airtable.com/v0/appi4RPkjYonAYvGA/shortcuts?maxRecords=3&view=Grid%20view",
+  airtableUrl = "https://api.airtable.com/v0/appi4RPkjYonAYvGA/shortcuts&view=Grid%20view",
   airtableApiKey = "keyDA3iMtSFG4Kj04";
